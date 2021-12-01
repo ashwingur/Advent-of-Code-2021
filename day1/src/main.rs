46ce7b0be1488095822595bed9edd940e2f1db9b
Ashwin <ashwingur88@gmail.com>
2021-12-01 17:49:34 +1100

    Finished 1.2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -9,6 +9,7 @@ where P: AsRef<Path> {
 }
 
 fn main() {
+    /* Part 1
     if let Ok(lines) = read_lines("input.txt") {
         // Iterator
         let mut count: i32 = 0;
@@ -24,5 +25,25 @@ fn main() {
         }
         println!("{}", count);
     }
+    */
+
+    if let Ok(lines) = read_lines("input.txt") {
+        // Iterator
+        let mut count: i32 = 0;
+        let mut vec = Vec::<i32>::new();
+        for line in lines {
+            if let Ok(ip) = line {
+                vec.push(ip.parse().unwrap());
+            }
+        }
+        for i in 3..vec.len() {
+            if vec[i] + vec[i - 1] + vec[i - 2] > vec[i - 1] + vec[i - 2] + vec[i - 3]{
+                count += 1
+            }
+        }
+        println!("{}", count);
+    }
+
+
 
 }
